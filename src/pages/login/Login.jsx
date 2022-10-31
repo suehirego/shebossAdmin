2aa70cce65b7628f674266c90693ed39132c3443
suehirego <hirego.susan@gmail.com>
2022-10-31 15:01:57 +0300

    edits

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
             e.preventDefault();
             dispatch({ type: "LOGIN_START" });
             try {
-                  const res = await axios.post("/auth/login", credentials);
+                  const res = await axios.post("https://shebossapi.herokuapp.com/api/auth/login", credentials);
                   if (res.data.isAdmin) {
                         dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
                         navigate("/");
