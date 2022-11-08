2f07da66f4abcb8823c29c03d12164b2ff1eaaf9
suehirego <hirego.susan@gmail.com>
2022-11-08 18:24:19 +0300

    edits

--- src/config.js
+++ src/config.js
@@ -1,8 +1,23 @@
+// import axios from 'axios'
+
+// export const axiosInstance = axios.create({
+//       baseURL: "https://shebossapi.herokuapp.com/api/",
+      
+// });
+
 import axios from 'axios'
 
+const BASE_URL = "https://shebossapi.herokuapp.com/api/";
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
+
 export const axiosInstance = axios.create({
-      baseURL: "https://shebossapi.herokuapp.com/api/",
-      
+      baseURL: BASE_URL,
+      header: { token: `Bearer ${TOKEN}` },
 });
+
 
 
