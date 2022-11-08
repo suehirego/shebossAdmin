8b9ed55c18985d8df8425cf7e985d8cbdbdb40d2
suehirego <hirego.susan@gmail.com>
2022-11-08 16:55:41 +0300

    edits

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-      baseURL: "https://shebossapi.herokuapp.com/api/"
+      baseURL: "https://shebossapi.herokuapp.com/api/",
 });
 
 
